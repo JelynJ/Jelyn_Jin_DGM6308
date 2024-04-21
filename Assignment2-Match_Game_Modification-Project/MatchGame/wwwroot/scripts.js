ab85b5921d2a36814725c3d7aad9295db983881e
JelynJ <149837668+JelynJ@users.noreply.github.com>
2024-04-21 15:55:59 -0400

    Add files via upload

---
+++ Assignment2-Match_Game_Modification-Project/MatchGame/wwwroot/scripts.js
@@ -0,0 +1,10 @@
+﻿<script>
+    function updateGridLayout(gridTemplateColumns) {
+        const gridContainer = document.querySelector('.grid-container');
+    gridContainer.style.gridTemplateColumns = gridTemplateColumns;
+    // Define a function named updateGridLayout
+    // Get the HTML element with the class name '.grid-container' using document.querySelector
+    // Set the gridTemplateColumns style property of the gridContainer element
+    // This property controls the size and number of columns in the grid layout
+    }
+</script>
