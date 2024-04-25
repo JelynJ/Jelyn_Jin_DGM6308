271b0b4724c1714a90ab4bb42ad313b5858e7095
JelynJ <149837668+JelynJ@users.noreply.github.com>
2024-04-25 00:04:21 -0400

    Add files via upload

---
+++ Human/Human/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            YourName Jelyn = new YourName("Jelyn", "Jin", "5'04", 108, 24);
+            YourName Mimi = new YourName("Mimi", "Cat", "2'62", 40, 3);
+
+            Jelyn.introduction();
+            Mimi.introduction();
+
+            YourName Dog = new YourName();
+            Dog.introduction();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Human/Human/YourName.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Human
+{
+	public class YourName
+	{
+		public string firstname = "";
+		public string lastname = "";
+		public string height = "";
+		public int weight = 0;
+		public int age = 0;
+
+		public YourName()
+		{
+			Console.WriteLine("Your Name was Called");
+		}
+		public YourName(string firstname, string lastname, string Height, int Weight, int Age)
+		{
+			this.firstname = firstname;
+			this.lastname = lastname;
+			this.height = Height;
+			this.weight = Weight;
+			this.age = Age;
+		}
+
+		public void introduction()
+		{
+			if (age >= 18)
+				Console.WriteLine("Hello my name is {0} {1}, I am {2} and weigh {3}. Also I am {4} years old.", firstname, lastname, height, weight, age);
+			else
+				Console.WriteLine("Sorry I can't introduce myself, I am underage.");
+
+		}
+	}
+}
+
